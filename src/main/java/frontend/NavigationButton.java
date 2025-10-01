79b8e69bf282b97a3a22fe76e219e35e7f38d666
SebWimm <sebastian.wimmershoff@hs-duesseldorf.de>
2025-10-01 20:10:56 +0200

    AB#28 added NavigationButton

---
+++ src/main/java/frontend/NavigationButton.java
@@ -0,0 +1,12 @@
+package frontend;
+
+import javafx.scene.control.Button;
+import models.Route;
+
+public class NavigationButton extends Button {
+
+    public NavigationButton (String label, Route target, Navigator navigator) {
+        super(label);
+        setOnAction(e -> navigator.show(target));
+    }
+}
