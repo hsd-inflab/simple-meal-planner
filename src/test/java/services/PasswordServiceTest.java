71668e7312e67dba5d09340bbdd1b88467341730
SebWimm <sebastian.wimmershoff@hs-duesseldorf.de>
2026-03-18 16:02:44 +0100

    added archunit to maven lifecycle

--- src/test/java/services/PasswordServiceTest.java
+++ src/test/java/services/PasswordServiceTest.java
@@ -1,11 +1,10 @@
-package util;
+package services;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import services.PasswordService;
 
 public class PasswordServiceTest {
     PasswordService pwService;
