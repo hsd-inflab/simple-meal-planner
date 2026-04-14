cc047f07a37edc90b208f259d4b72842cfb1dc8e
SebWimm <sebastian.wimmershoff@hs-duesseldorf.de>
2026-04-14 17:40:51 +0200

    removed deprecated class and test for db implementation

--- src/test/java/hsd/inflab/smp/MealPlannerApplicationTest.java
+++ src/test/java/hsd/inflab/smp/MealPlannerApplicationTest.java
@@ -3,10 +3,12 @@ package hsd.inflab.smp;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import hsd.inflab.smp.service.*;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class MealPlannerApplicationTest {
 
