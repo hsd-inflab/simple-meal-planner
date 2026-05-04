9ba5990b4056a1aa4879b1a9e1fda577eae1002b
Marvin Häntzschel <marvin.haentzschel@study.hs-duesseldorf.de>
2026-05-04 14:29:31 +0200

    BUG-FIX: resolved comments

--- src/main/java/hsd/inflab/smp/config/SecurityConfig.java
+++ src/main/java/hsd/inflab/smp/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
